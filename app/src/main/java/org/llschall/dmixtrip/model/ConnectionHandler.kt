f8f99bde78b4488773b370bd79c5c0eab75a9487
llschall <llschall@gmx.com>
2024-05-31 21:32:13 +0200

    [Connection] Check bluetooth permission

--- app/src/main/java/org/llschall/dmixtrip/model/ConnectionHandler.kt
+++ app/src/main/java/org/llschall/dmixtrip/model/ConnectionHandler.kt
@@ -1,8 +1,11 @@
 package org.llschall.dmixtrip.model
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class ConnectionHandler {
@@ -16,16 +19,25 @@ class ConnectionHandler {
 
     fun setup(): String {
         if (context == null) return "context not found"
+        if (ActivityCompat.checkSelfPermission(
+                context!!,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return "Permission not granted"
+        }
+
         try {
             service = ContextCompat.getSystemService(context!!, BluetoothManager::class.java)
             if (service == null) return "service not found"
             adapter = service!!.adapter
+
             if (adapter == null) return "adapter not found"
+            return "found adapter ${adapter!!.name}"
         } catch (e: Exception) {
             if (e.message == null) return "unknown error"
             return e.message!!
         }
-        return "setup ok"
     }
 
 }
